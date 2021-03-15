366af66ee8a87fd0e2314c5d8d42c63be4ed572c
RVeronica <akinrv16@gmail.com>
2021-03-15 14:41:29 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+  let num = require('num-words');
+  let readableNum = num(number);
+  let finalReadableNum='';
+  let readableNumArr = readableNum.split(' ');
+  for(let i =0; i<readableNumArr.length;i++){
+      if(readableNumArr[i]!='and'){
+          if(i!=readableNumArr.length-1) {
+              finalReadableNum = finalReadableNum + readableNumArr[i] + ' ';
+          } else{
+              finalReadableNum = finalReadableNum + readableNumArr[i];
+          }
+
+      } else {
+          readableNumArr.splice(i,0);
+      }
+  }
+
+  return finalReadableNum;
 }
